81d5a996afaef16e804a533c2b334bb700fc01a6
Erek Röös <erek.roeoes@outlook.de>
2019-01-20 19:07:35 +0100

    Basic setup

---
+++ scripts/create-token.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = process.env
+
+const signedJWT = jwt.sign(
+  {
+    id: 'Application Identity',
+    name: 'Application Identity',
+    scope: 'app'
+  },
+  JWT_SECRET
+)
+
+console.log(signedJWT)
