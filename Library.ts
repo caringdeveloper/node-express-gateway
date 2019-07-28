f0c8affa7b744f39e2efc891623592f7f1c13a08
Erek Röös <erek.roeoes@outlook.de>
2019-07-28 13:56:44 +0200

    First OOP version of the gateway

---
+++ Library.ts
@@ -0,0 +1,58 @@
+/*
+    Copyright(c) 2019 Erek Röös. All rights reserved.
+
+    The MIT License(MIT)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files(the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+import * as cors from "cors";
+import * as bodyParser from "body-parser";
+import { injectable, inject } from "inversify";
+import { Application } from "express";
+
+import IConfigurationProvider from "./src/IConfigurationProvider";
+import IRouteGenerator from "./src/IRouteGenerator";
+
+@injectable()
+export default class Library {
+  @inject("App")
+  private app: Application;
+
+  @inject("ConfigurationProvider")
+  private configurationProvider: IConfigurationProvider;
+
+  @inject("RouteGenerator")
+  private routeGenerator: IRouteGenerator;
+
+  public async run(configFilePath: string): Promise<void> {
+    await this.configurationProvider.readConfiguration(configFilePath);
+    const config = this.configurationProvider.getConfiguration();
+
+    this.app.enable("trust proxy");
+    this.app.use(cors());
+    this.app.use(bodyParser.json({ limit: "50MB" }));
+
+    if (config.routes) {
+      config.routes.forEach(route => this.routeGenerator.createProxyRoute(route));
+    }
+
+    this.app.listen(config.port, () => console.log("[INFO]", "Gateway is listening"));
+  }
+}
---
+++ src/RouteGenerator.ts
@@ -0,0 +1,141 @@
+/*
+    Copyright(c) 2019 Erek Röös. All rights reserved.
+
+    The MIT License(MIT)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files(the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+import axios from "axios";
+import * as pathToRegexp from "path-to-regexp";
+import { injectable, inject } from "inversify";
+import { Application, Request, Response, NextFunction } from "express";
+
+import IRouteGenerator from "./IRouteGenerator";
+import IConfigurationProvider from "./IConfigurationProvider";
+import { Route, Configuration, GatewayRequest } from "./models/Configuration";
+import { authentication } from "./middleware/authentication";
+import { authorization } from "./middleware/authorization";
+
+@injectable()
+export default class RouteGenerator implements IRouteGenerator {
+  @inject("App")
+  private app: Application;
+
+  @inject("ConfigurationProvider")
+  private configurationProvider: IConfigurationProvider;
+
+  private configuration: Configuration = undefined;
+
+  public async createProxyRoute(route: Route): Promise<void> {
+    if (!this.configuration)
+      this.configuration = await this.configurationProvider.getConfiguration();
+
+    const toPath = pathToRegexp.compile(route.downstreamPath);
+
+    const routeProxy = (method: string) => async (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) => {
+      try {
+        let URL = route.downstreamSSL
+          ? "https://" + route.downstreamHost + ":" + route.downstreamPort
+          : "http://" + route.downstreamHost + ":" + route.downstreamPort;
+
+        URL += toPath(req.params);
+        URL += req.url.split("?")[1] !== undefined ? "?" + req.url.split("?")[1] : "";
+
+        if (route.downstreamUrlSuffix) {
+          const parts = req.url.split("?");
+          let queryString = parts[1];
+
+          URL += queryString ? "&" + route.downstreamUrlSuffix : "?" + route.downstreamUrlSuffix;
+        }
+
+        let data;
+        if (method === "post" || method === "put" || method === "patch") {
+          delete req.headers.host;
+
+          const response = await axios[method](URL, req.body, {
+            headers: {
+              ...req.headers,
+              "x-user-info": JSON.stringify(req["user"] || null),
+              "Cache-Control": "no-cache"
+            },
+            params: req.query
+          });
+
+          data = response.data;
+        } else {
+          delete req.headers.host;
+
+          const response = await axios[method](URL, {
+            headers: {
+              ...req.headers,
+              "x-user-info": JSON.stringify(req["user"] || null),
+              "Cache-Control": "no-cache"
+            },
+            params: req.query
+          });
+
+          data = response.data;
+        }
+
+        if (typeof data === "string") {
+          res.status(200).send(data);
+        } else {
+          return res.status(200).json(data);
+        }
+      } catch (err) {
+        const filteredError = {
+          Address: `${err.address}:${err.port}`,
+          ToUrl: err.config.url,
+          StatusCode: err.response && err.response.status,
+          Headers: err.config.headers.authorization,
+          Method: err.config.method,
+          Params: err.config.params,
+          Body: err.response && err.response.body,
+          Data: err.response && err.response.data
+        };
+
+        if (err.response && err.response.status) {
+          return res.status(err.response.status).send(err.response.data);
+        }
+
+        return res.status(500).send();
+      }
+    };
+
+    let middlewares: Function[] = [];
+
+    if (route.auth) {
+      middlewares.push(authentication(this.configuration));
+      middlewares.push(authorization(route.scopes));
+    }
+
+    route.upstreamMethods.forEach(method => {
+      this.app[method](route.upstreamPath, middlewares, routeProxy(method));
+    });
+  }
+
+  public createAggregateRoute(route: Route): void {
+    throw new Error("Method not implemented.");
+  }
+}
